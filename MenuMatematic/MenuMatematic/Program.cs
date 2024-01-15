617dbad2079c8e9e02642880e493e7ed7cd57320
Carla Segura <a2308761@institutmontilivi.cat>
2024-01-15 12:21:02 +0100

    inici del treball

---
+++ MenuMatematic/MenuMatematic/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace MenuMatematic;
+
+class Program
+{
+    //Main
+    static void Main(string[] args)
+    {
+        int numEntrada;
+
+        Console.WriteLine("1. Maxim");
+        Console.WriteLine("2. MCD");
+        Console.WriteLine("3. MCM");
+        Console.WriteLine("4. Factorial");
+        Console.WriteLine("5. Combinatori");
+        Console.WriteLine("6. Major DivisorMajor");
+        Console.WriteLine("7. Comprovar Primer");
+        Console.WriteLine("8. Num Primers");
+
+        switch (numEntrada)
+        {
+            case 1: 
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+
+            
+
+        }
+
+
+
+        /*if (numEntrada == 1)
+        {
+            
+        }
+        else if (numEntrada == 2)
+        {
+
+        }
+        else if (numEntrada == 3)
+        {
+
+        }
+        else if (numEntrada == 4)
+        {
+
+        }
+        else if (numEntrada == 5)
+        {
+
+        }
+        else if (numEntrada == 6)
+        {
+
+        }
+        else if (numEntrada == 6)
+        {
+
+        }*/
+
+    }
+
+    //Console.WriteLine(Metode(Variable));
+
+    //Maxim
+    static int maxim(int numEntrada)
+    {
+
+    }
+
+    //mcd
+    static int mcd(int num1)
+    {
+
+    }
+
+    //mcm
+    static int mcm(int num1)
+    {
+
+    }
+
+    //Factorial
+    static int factorial(int num1)
+    {
+
+    }
+
+    //Combinatori
+    static int Maxim(int num1)
+    {
+
+    }
+
+    //MostrarDivisorMajor
+    static int Maxim(int num1)
+    {
+
+    }
+
+    //EsPrimer
+    static int Maxim(int num1)
+    {
+
+    }
+
+    //NPrimerPrimers
+    static int Maxim(int num1)
+    {
+
+    }
+}
+
+/* 
+ * Contador:
+ * while(i!=0)
+ * {
+ * Console.Write("\r")
+ * Console.Write($"Tornant al menu: {i}s")
+ * Thread.Sleep(1000);
+ * i--
+ * }
+*/
