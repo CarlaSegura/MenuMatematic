4fd423e9dd53775e34060dfa09a85580b59d0024
Carla Segura <a2308761@institutmontilivi.cat>
2024-01-18 09:55:56 +0100

    Primera hora de treball

--- MenuMatematic/MenuMatematic/Program.cs
+++ MenuMatematic/MenuMatematic/Program.cs
@@ -2,89 +2,116 @@
 
 class Program
 {
-    //Main
-    static void Main(string[] args)
+    //Menu
+    static void Menu()
     {
-        int numEntrada;
+        int numeroMenu;
 
         Console.WriteLine("1. Maxim");
         Console.WriteLine("2. MCD");
         Console.WriteLine("3. MCM");
         Console.WriteLine("4. Factorial");
         Console.WriteLine("5. Combinatori");
-        Console.WriteLine("6. Major DivisorMajor");
+        Console.WriteLine("6. Major Divisor");
         Console.WriteLine("7. Comprovar Primer");
         Console.WriteLine("8. Num Primers");
+        Console.WriteLine("Clica la q per sortir");
 
-        switch (numEntrada)
+        Console.WriteLine("Fica un numero");
+        numeroMenu = Console.ReadLine(ValorsEntrada(numeroMenu));
+    }
+
+   static int ComprovarNum()
+    {
+        if (num)
+    }
+
+    //ValorsEntrada
+    static int ValorsEntrada(int numeroMenu)
+    {
+        int numero1, numero2;
+
+        switch (numeroMenu)
         {
             case 1:
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
+                Maxim(ref numero1, ref numero2);
+                Console.WriteLine($"El numero", numero1, "és el mes gran");
+                break;
 
             case 2:
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
+                break;
+
             case 3:
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
+                break;
+
             case 4:
+                numero1 = DemanarValors();
+
             case 5:
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
+
             case 6:
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
+
             case 7:
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
+
             case 8:
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
+
             case 9:
-        }
+                numero1 = DemanarValors();
+                numero2 = DemanarValors();
 
-
-
-        /*if (numEntrada == 1)
-        {
-            
-        }
-        else if (numEntrada == 2)
-        {
+            default:
 
         }
-        else if (numEntrada == 3)
-        {
+    }
 
-        }
-        else if (numEntrada == 4)
-        {
-
-        }
-        else if (numEntrada == 5)
-        {
-
-        }
-        else if (numEntrada == 6)
-        {
-
-        }
-        else if (numEntrada == 6)
-        {
-
-        }*/
-
+    static int DemanarValors()
+    {
+        int numero;
+        Console.Clear();
+        Console.Write("Fica un valor");
+        numero = Console.Read();
+        return numero;
     }
 
     //Console.WriteLine(Metode(Variable));
 
     //Maxim
-    static int maxim(int numEntrada)
+    static void Maxim(ref int numero1, ref int numero2)
     {
-
+        if (numero2 > numero1)
+        {
+            (numero1, numero2) = (numero2, numero1);
+        }
     }
 
     //mcd
-    static int mcd(int num1)
+    static int Mcd(int num1)
     {
 
     }
 
     //mcm
-    static int mcm(int num1)
+    static int Mcm(int num1)
     {
 
     }
 
     //Factorial
-    static int factorial(int num1)
+    static int Factorial(int num1)
     {
 
     }
@@ -114,13 +141,31 @@ class Program
     }
 }
 
-/* 
- * Contador:
- * while(i!=0)
- * {
- * Console.Write("\r")
- * Console.Write($"Tornant al menu: {i}s")
- * Thread.Sleep(1000);
- * i--
- * }
-*/
+static int IntroduirValor()
+{
+    Console.Clear();
+    Console.Write("Escriu un Valor");
+    
+}
+
+
+static void return()
+    {
+        int i = 5;
+        while(i!=0)
+        {
+            Console.Write("\r")
+            Console.Write($"Tornant al menu: {i}s")
+            Thread.Sleep(1000);
+            i--
+        }
+        Menu();
+    }
+
+
+
+
+static void Main(string[] args)
+{
+
+}
