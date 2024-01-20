e4379ea060bea1b67529045653dc20840b0d8169
Carla Segura <a2308761@institutmontilivi.cat>
2024-01-20 16:49:29 +0100

    Casi acabat els metodes

--- MenuMatematic/MenuMatematic/Program.cs
+++ MenuMatematic/MenuMatematic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MenuMatematic;
 
@@ -58,47 +59,51 @@ class Program
                 numero1 = DemanarValors();
                 numero2 = DemanarValors();
                 Maxim(ref numero1, ref numero2);
-                Console.WriteLine($"El numero", numero1, "és el mes gran");
+                Console.WriteLine($"El numero {numero1} és el mes gran");
                 break;
 
             case '2':
                 numero1 = DemanarValors();
                 numero2 = DemanarValors();
-                Mcd(ref numero1, ref numero2);
-                Console.WriteLine()
+                int mcd = Mcd(numero1, numero2);
+                Console.WriteLine($"El MCD de {numero1} i {numero2} és: {mcd}");
                 break;
 
             case '3':
                 numero1 = DemanarValors();
                 numero2 = DemanarValors();
+                int mcm = Mcm(numero1, numero2);
+                Console.WriteLine($"El MCM de {numero1} i {numero2} és: {mcm}");
                 break;
 
             case '4':
                 numero1 = DemanarValors();
+                int factorial = Factorial(numero1);
+                Console.WriteLine($"El Factorial de {numero1} és: {factorial}");
                 break;
 
             case '5':
                 numero1 = DemanarValors();
+                int combinatori = Combinatori(numero1);
+                Console.WriteLine($"El Factorial de {numero1} és: {combinatori}");
                 numero2 = DemanarValors();
                 break;
 
             case '6':
                 numero1 = DemanarValors();
+                //MostrarDivisorMajor
                 numero2 = DemanarValors();
                 break;
 
             case '7':
                 numero1 = DemanarValors();
                 numero2 = DemanarValors();
+                //EsPrimer
                 break;
 
             case '8':
                 numero1 = DemanarValors();
-                numero2 = DemanarValors();
-                break;
-
-            case '9':
-                numero1 = DemanarValors();
+                //NPrimerPrimers
                 numero2 = DemanarValors();
                 break;
         }
@@ -116,7 +121,7 @@ class Program
     //Console.WriteLine(Metode(Variable));
 
     //Maxim
-    static void Maxim(ref int numero1, ref int numero2)
+    static void Maxim (ref int numero1, ref int numero2)
     {
         if (numero2 > numero1)
         {
@@ -125,7 +130,7 @@ class Program
     }
 
     //mcd
-    static void Mcd(ref int numero1, ref int numero2)
+    static int Mcd(int numero1, int numero2)
     {
         while (numero1 != 0)
         {
@@ -133,46 +138,78 @@ class Program
             numero2 = numero1 % numero2;
             numero1 = temp;
         }
+        return numero1;
     }
 
     //mcm
-    static int Mcm(int num1)
+    static int Mcm(int numero1, int numero2)
     {
-        //Debes de utilizar si o si el mcd
-        if (a == 0 || b == 0)
+        if (numero1 == 0 || numero2 == 0)
         {
             return 0;
         }
 
-        return (a * b) / CalcularMCD(a, b);
+        return (numero1 * numero2) / Mcd(numero1, numero2);
     }
 
     //Factorial
-    static int Factorial(int num1)
+    static int Factorial(int numero1)
     {
-
+        int res = 1;
+        for (int i = 1; i <= numero1; i++)
+        {
+            res = res * i;
+        }
+        return res;
     }
 
     //Combinatori
-    static int Combinatori(int num1)
+    static int Combinatori(int numero1, int numero2)
     {
-
+        if (numero1 < 0 || numero2 < 0 || numero2 > numero1)
+        {
+           //Tornar menu
+        }
+        int res = Factorial(numero1) / (Factorial(numero2) * Factorial(numero1 - numero2));
+        return res;
     }
 
     //MostrarDivisorMajor
-    static int MostrarDivisorMajor(int num1)
+    static int MostrarDivisorMajor(int numero1)
     {
-
+        if (numero1 <= 0)
+        {
+            //Tornar menu
+        }
+        //Falta codi
     }
 
     //EsPrimer
-    static int EsPrimer(int num1)
+    static bool EsPrimer(int numero1)
     {
+        bool primer = false;
+        int cont2 = 1;
 
+        for (int i = 1; i <= numero1; i++)
+        {
+            if (numero1 % cont2 == 0)
+            {
+                cont2++;
+            }
+        }
+
+        if (cont2 == 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     //NPrimerPrimers
-    static int NPrimerPrimers(int num1) //Poses un numero i ha de dir-te tots els numeros primers avans que aquell numero
+    static int NPrimerPrimers(int numero1) //Poses un numero i ha de dir-te tots els numeros primers avans que aquell numero
     {
 
     }
